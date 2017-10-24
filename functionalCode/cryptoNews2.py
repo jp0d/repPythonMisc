5c721c94b34f66cb368e97a4a8f8e1de8ac5ad38
jp0d <jajati.k@gmail.com>
2017-10-24 16:57:07 +1100

    cryptoNews2.py

--- functionalCode/cryptoNews2.py
+++ functionalCode/cryptoNews2.py
@@ -19,4 +19,18 @@ import bs4
 import numpy as np
 import pandas as pd
 
+def coinNews(coin, element):
+	urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+	url = 'https://www.google.com.au/search?safe=active&tbm=nws&q=' + coin + '+coin'
+	res = requests.get(url, verify=False)
+	data = bs4.BeautifulSoup(res.text, 'html.parser')
 
+	for d in data.select(element):
+		print(d.getText())
+
+
+def main(argv):
+	coinNews(argv[1], argv[2])
+
+if __name__ == "__main__":
+	main(sys.argv)
