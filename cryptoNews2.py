a75c2421c0d716aac84e742934c2e9955d3eb813
jp0d <jajati.k@gmail.com>
2017-10-24 16:54:08 +1100

    cryptoNews2.py

---
+++ cryptoNews2.py
@@ -0,0 +1,22 @@
+# import system related libraries
+import os
+import io
+import math
+import shutil
+import traceback
+import logging
+import sys
+# import time and date related libraries
+from datetime import datetime
+from datetime import timedelta
+import time
+# import web interaction libraries
+import requests
+import json
+import urllib
+import urllib3
+import bs4
+import numpy as np
+import pandas as pd
+
+
---
+++ functionalCode/timestamp.py
@@ -0,0 +1,19 @@
+import os
+import io
+import time
+from datetime import datetime
+import requests
+import json
+import urllib
+import bs4
+import math
+import numpy as np
+import pandas as pd
+
+def main():
+    url = 'https://min-api.cryptocompare.com/data/pricehistorical?fsym=SC&tsyms=BTC,USD,AUD&ts='
+    t = datetime.datetime(1452680400)
+
+
+if __name__ == "__main__":
+    main()
